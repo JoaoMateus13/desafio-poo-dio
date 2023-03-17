3321acb3c7d036a04ea001b0371f920de3e319f1
61907813306 <jms.lopes@discente.ufma.br>
2023-03-17 14:05:42 -0300

    mentoria e curso

---
+++ src/src/Main.java
@@ -0,0 +1,18 @@
+import dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        Curso curso1 = new Curso();
+
+
+
+        System.out.println(curso1);
+
+
+
+
+    }
+}
